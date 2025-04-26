f3dcde25037ae600dffd9947c80c5b023fbbf085
JanteWinkel <janpaultewinkelj@gmail.com>
2025-04-26 13:11:51 -0400

    feat: cambios desuda general

--- app/finanzas/components/deudas.tsx
+++ app/finanzas/components/deudas.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 ; // Deuda General
-const deudaPrimeraEtapa = 37678.93; // Deuda de la Primera Etapa
-const deudaSegundaEtapa = 15221.88; // Deuda de la Segunda Etapa
+const deudaPrimeraEtapa = 33529.06; // Deuda de la Primera Etapa
+const deudaSegundaEtapa = 13430.97; // Deuda de la Segunda Etapa
 
 const Deudas = () => {
     return (
         <div className="mt-12">
             <h2 className="text-2xl font-semibold text-primary mb-1">DEUDA GENERAL</h2>
-            <h4 className="font-semibold  mb-4">Fecha (11/4/2025)</h4>
+            <h4 className="font-semibold  mb-4">Fecha (26/4/2025)</h4>
             <div className="flex flex-wrap gap-6 justify-center">
                 <div className="w-full sm:w-1/4 lg:w-1/5 p-3 border border-red-400 rounded-lg shadow bg-white dark:bg-gray-800 dark:border-red-400 text-center hover:scale-110">
                     <h3 className="text-lg font-medium text-gray-500 dark:text-gray-400 mb-2">DEUDA GENERAL</h3>
