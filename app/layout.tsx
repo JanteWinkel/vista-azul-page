21e980bd2a603dc8ff42428bb249aae4a5718922
JanteWinkel <janpaultewinkelj@gmail.com>
2025-06-04 14:05:59 -0400

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Metadata } from 'next';
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({children}: { children: React.ReactNode } ) {
     
       <html lang='en'>
         <body className={`${inter.className} min-h-screen bg-background font-sans antialiased`}>
+        <Analytics/>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
