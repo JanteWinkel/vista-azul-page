a4696663e7c3761fe4c1629134510e570336d52c
JanteWinkel <janpaultewinkelj@gmail.com>
2025-12-04 15:43:44 -0400

    encendido de navidad

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
 'use client'
-import BannerPerfil from "@/components/banner-perfil";
-import CarouselFotos from "@/components/carousel-fotos";
 import Eventos from "@/components/accesos-directos";
 import Footer from "@/components/footer";
-import BannerInfo from "@/components/banner-informacion";
 import DiaTrabajador from "@/components/trabajador";
 import Informe2425 from "./informes/components/informe_2024-2025";
 import AnuncioCenso from "@/components/anuncio_censo_mascotas";
 import InformesAsamblea from "./informes/components/informes";
 import InfoUbicacion from "@/components/direccion";
 import AnuncioReutilizable from "@/components/anuncio-reutilizable-1";
+import BannerNavidad from "@/components/banner-navidad";
 
 export default function Home() {
   return (
     <main >
       <div>
-
+      <BannerNavidad />
         <AnuncioCenso />
         <AnuncioReutilizable />
-        <BannerPerfil />
-        <CarouselFotos />
-        <BannerInfo />
+
         <InfoUbicacion />
         <InformesAsamblea />
         <Informe2425 />
